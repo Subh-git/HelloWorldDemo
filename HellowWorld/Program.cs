d2c2cc1fb6d06098fd1bec8efd38ef7283db3c40
Subh-git <90467867+Subh-git@users.noreply.github.com>
2021-10-05 08:15:28 +0530

    New changes in remote

--- HellowWorld/Program.cs
+++ HellowWorld/Program.cs
@@ -17,7 +17,7 @@ namespace HellowWorld
         {
             Console.WriteLine("Addition Operation");
             Console.WriteLine("Modifying");
-            Console.WriteLine("New conflict at local")
+            Console.WriteLine("New conflict at remote")
         }
     }
 }
